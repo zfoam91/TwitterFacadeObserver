2e094f791ce5e36087131e636fe268198b480ba5
zfoam91 <36771609+zfoam91@users.noreply.github.com>
2022-04-14 16:33:20 +0400

    Add files via upload

---
+++ TwitterTask/Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using Twitter;
+using System;
+TwitterProfile Profile1 = new TwitterProfile("User1");
+TwitterProfile Profile2 = new TwitterProfile("User2");
+TwitterProfile Profile3 = new TwitterProfile("User3");
+TwitterProfile Profile4 = new TwitterProfile("User4");
+TwitterProfile Profile5 = new TwitterProfile("User5");
+
+Profile1.subscribe(Profile2);
+Profile1.subscribe(Profile3);
+Profile2.subscribe(Profile1);
+Profile4.subscribe(Profile1);
+Profile5.subscribe(Profile1);
+Profile2.subscribe(Profile2);
+
+Profile1.ShowSubscribers();
+Profile1.ShowSubscriptions();
+
+Profile1.SetStatus(":D");
+Profile4.unsubscribe(Profile1);
+Profile1.SetStatus("D:");
+
+Profile1.ShowSubscribers();
+
+
